744c14577e21b48302662aeb1ebe912b4ac52034
Lucas Wall <wall.lucas@gmail.com>
2024-09-27 15:08:35 -0300

    Turn sensors on and off with relays.

--- hydro01/hydro01.ino
+++ hydro01/hydro01.ino
@@ -8,6 +8,9 @@ const uint8_t sensorTdsPin = 39;
 const uint8_t sensorPhPin = 36;
 const uint8_t sensorWaterLevel = 34;
 
+const uint8_t switchTdsPin = 4;
+const uint8_t switchPhPin = 5;
+
 OneWire oneWireTemp(sensorTempPin);
 DallasTemperature sensorTemp(&oneWireTemp);
 
@@ -25,13 +28,20 @@ void setup() {
   pinMode(sensorTdsPin, INPUT);
   pinMode(sensorPhPin, INPUT);
   pinMode(sensorWaterLevel, INPUT);
+  pinMode(switchTdsPin, OUTPUT);
+  digitalWrite(switchTdsPin, HIGH);
+  pinMode(switchPhPin, OUTPUT);
+  digitalWrite(switchPhPin, HIGH);
 }
 
 void loop() {
+  Serial.println(F("======= START READ =============="));
   const float temp = readTemperature();
   const float ppm = readPpm(temp);
+  delay(2000);
   const float ph = readPH(temp);
   const float waterLevel = readWaterLevel();
+  Serial.println(F("======= RESULT =================="));
   Serial.print(F("Sensors "));
   Serial.print(temp);
   Serial.print(F(" °C, "));
@@ -41,7 +51,8 @@ void loop() {
   Serial.print(F(" pH, "));
   Serial.print(waterLevel);
   Serial.println(F(" %wl"));
-  delay(1000);
+  Serial.println(F("================================="));
+  delay(5000);
 }
 
 float readTemperature() {
@@ -74,11 +85,14 @@ float readVoltage(uint8_t pin) {
 
 float readPpm(float temperature) {
   Serial.print(F("TDS "));
+  digitalWrite(switchTdsPin, LOW);
+  delay(3000);
   const float voltage = readVoltage(sensorTdsPin);
   const float compC = 1.0 + 0.02 * (temperature - 25.0);
   const float compV = voltage / compC;
   const float tdsValue = (133.42 * compV * compV * compV - 255.86 * compV * compV + 857.39 * compV) * TdsFactor;
   Serial.print(F(", ppm = ")); Serial.println(tdsValue);
+  digitalWrite(switchTdsPin, HIGH);
   return tdsValue;
 }
 
@@ -86,9 +100,12 @@ float readPH(float temperature) {
   const float lowPH = 4.01; const float lowV = 2.00;
   const float midPH = 6.86; const float midV = 1.66;
   Serial.print(F("PH "));
+  digitalWrite(switchPhPin, LOW);
+  delay(3000);
   const float voltage = readVoltage(sensorPhPin);
   const float phValue = midPH + (voltage - midV) * ((midPH - lowPH) / (midV - lowV)) + 0.03 * (temperature - RefTemperature);
   Serial.print(F(", pH = ")); Serial.println(phValue);
+  digitalWrite(switchPhPin, HIGH);
   return phValue;
 }
 
