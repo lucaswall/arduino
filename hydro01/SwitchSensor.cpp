7252eec8f1c3614dcecc751965c4c566793ab3bf
Lucas Wall <wall.lucas@gmail.com>
2024-10-03 17:49:03 -0300

    Log on sensor switch on and off.

--- hydro01/SwitchSensor.cpp
+++ hydro01/SwitchSensor.cpp
@@ -15,10 +15,14 @@ void SwitchSensor::init()
 
 void SwitchSensor::on()
 {
+    Serial.print(F("SwitchSensor::on pin "));
+    Serial.println(pin);
     digitalWrite(pin, LOW);
 }
 
 void SwitchSensor::off()
 {
+    Serial.print(F("SwitchSensor::off pin "));
+    Serial.println(pin);
     digitalWrite(pin, HIGH);
 }
